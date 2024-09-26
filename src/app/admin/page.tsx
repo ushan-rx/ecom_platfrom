6fe9af07a2033128dd9e1c0448a5398efdd74295
ushan senarathna <ushansenarathna000@gmail.com>
2024-09-26 23:24:05 +0530

    add product from created

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -56,7 +56,7 @@ export default async function AdminDashboard(){
         getProductData()
     ])
     
-    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
+    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
        <DashboardCard 
             title="Products"
             description={formatNumber(salesData.numberOfSales)}
