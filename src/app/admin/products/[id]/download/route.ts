80140dd9b0f13f39fb3428085c74b0d5a30d8b8e
ushan senarathna <ushansenarathna000@gmail.com>
2024-09-30 12:43:13 +0530

    product download completed

---
+++ src/app/admin/products/[id]/download/route.ts
@@ -0,0 +1,28 @@
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises"
+
+export async function GET(req:NextRequest, {params:{id}}:{params:{id:string}}){
+    const product = await db.product.findUnique({
+        where:{id},
+        select:{
+            filePath:true,
+            name:true,
+        },
+    })
+
+    if(product == null) return notFound()
+
+    const {size} = await fs.stat(product.filePath);
+    const file = await fs.readFile(product.filePath);
+    const extension =  product.filePath.split(".").pop();
+
+    return new NextResponse(file, {
+        headers: {
+            "content-Disposition": `attachment; filename="${product.name}.${extension}"`,
+            "content-length": size.toString(),
+        }
+    })
+
+}
